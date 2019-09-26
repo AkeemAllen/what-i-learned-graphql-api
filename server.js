cb1aeec6ea802f2a4e01354459b7544e47d6b717
Akeem Allen <allenakeem8@protonmail.com>
2019-09-25 20:22:51 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const express_graphql = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+var root = {
+  message: () => "Hello World"
+};
+
+var app = express();
+
+app.use(
+  "/graphql",
+  express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+app.listen(8081, () => {
+  console.log("Server is running on port 8081");
+});
