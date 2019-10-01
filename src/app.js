381928f937105d487c7354abeeb3ceb0135f6038
Akeem Allen <allenakeem8@protonmail.com>
2019-09-30 23:22:56 -0500

    Added loggin in morgan

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const express_graphql = require("express-graphql");
 const cors = require("cors");
 const { buildSchema } = require("graphql");
 const schema = require("./schema/schema");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 require("dotenv/config");
 
 const app = express();
@@ -12,6 +14,22 @@ const server = http.createServer(app);
 
 // allows cross-origin requests
 app.use(cors());
+
+app.use(bodyParser.json());
+
+// for logging purposes
+app.use(morgan("dev"));
+
+// setting headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // connect to mongo database
 mongoose.connect(process.env.MONGO_DB, {
