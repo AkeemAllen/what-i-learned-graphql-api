bc34fc3b4a3f0283426739c7ba72b54d6e2e93d6
Akeem Allen <allenakeem8@protonmail.com>
2019-10-20 15:58:59 -0500

    Changed working directory

--- .circleci/scripts/cpyToServer.sh
+++ .circleci/scripts/cpyToServer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp -r project/ ssh akeem@74.207.224.133:~/
+spawn scp -r what-i-learned-graphql-api/ ssh akeem@74.207.224.133:~/
 expect "akeem@74.207.224.133's password"
 send "akstar4321\r"
 interact
