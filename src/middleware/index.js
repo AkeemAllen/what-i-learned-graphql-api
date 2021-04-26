1991175b884c79cb7acf45bebb25610302568fb1
Akeem Allen <allenakeem8@gmail.com>
2021-04-26 06:07:17 -0500

    backing up projects

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -6,21 +6,21 @@ const graphqlHttp = require("express-graphql");
 const graphqlSchema = require("../graphql/schema");
 const graphqlResolver = require("../graphql/resolvers");
 
-exports.useMorgan = app => {
+exports.useMorgan = (app) => {
   app.use(morgan("dev"));
 };
 
 exports.mongoConnection = () => {
-  mongoose.connect(process.env.MONGO_DB, {
+  mongoose.connect("mongodb://localhost:27017/what-i-learned", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   });
   mongoose.connection.once("open", () => {
     console.log("conneted to database");
   });
 };
 
-exports.setHeaders = app => {
+exports.setHeaders = (app) => {
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
@@ -32,15 +32,15 @@ exports.setHeaders = app => {
   });
 };
 
-exports.useCors = app => {
+exports.useCors = (app) => {
   app.use(cors());
 };
 
-exports.useBodyParser = app => {
+exports.useBodyParser = (app) => {
   app.use(bodyParser.json());
 };
 
-exports.setUpGraphql = app => {
+exports.setUpGraphql = (app) => {
   app.use(
     "/graphql",
     graphqlHttp({
@@ -56,7 +56,7 @@ exports.setUpGraphql = app => {
         const message = err.message || "An error occured";
         const code = err.originalError.code || 500;
         return { message: message, status: code, data: data };
-      }
+      },
     })
   );
 };
